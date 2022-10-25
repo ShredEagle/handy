6968320183dabd0b0e24a59334dcca954ead78ad
Francois Poizat <francois.poizat@gmail.com>
2022-10-25 11:25:00 +0200

    update shred_conan_base to 0.0.5

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -34,5 +34,5 @@ class Handy(ConanFile):
     }
 
 
-    python_requires="shred_conan_base/0.0.3@adnn/stable"
+    python_requires="shred_conan_base/0.0.5@adnn/stable"
     python_requires_extend="shred_conan_base.ShredBaseConanFile"
