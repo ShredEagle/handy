5fb4067c56937ffc83129bb43e7c2a691efd7374
Adrien <Adnn@users.noreply.github.com>
2023-04-26 10:38:33 +0200

    Add a naïve MovableAtomic implementation.

---
+++ src/lib/handy/handy/MovableAtomic.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <atomic>
+
+
+namespace ad {
+
+
+/// \brief Hackish class, intended to compose movable types with atomic data members.
+/// \warning This is dangerous, for e.g. if a thread is using the moved from atomic
+/// to synchronise with a thread already using the moved to atomic (i.e. sync is lost)
+/// Yet this is useful if the move is only part of an initialization process.
+template <class T>
+struct MovableAtomic : public std::atomic<T>
+{
+    using std::atomic<T>::atomic;
+    using std::atomic<T>::operator=;
+
+    MovableAtomic(MovableAtomic && aOther) :
+        std::atomic<T>{aOther.load()}
+    {};
+};
+
+
+} // namespace ad
