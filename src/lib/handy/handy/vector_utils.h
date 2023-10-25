ef8f663bb00c0c7d0c83533ba6754dd81b4e327a
Adrien <Adnn@users.noreply.github.com>
2023-10-25 15:37:42 +0200

    Add utility to "list initialize" a vector of move-only types.

--- src/lib/handy/handy/vector_utils.h
+++ src/lib/handy/handy/vector_utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <vector>
 
 namespace ad {
@@ -8,6 +9,28 @@ namespace ad {
     std::size_t getStoredSize(const std::vector<T_element> & aVector)
     {
         return sizeof(T_element) * aVector.size();
+    }
+
+    
+    // see: https://stackoverflow.com/a/42915152/1027706
+    template <class T_element, std::size_t N_length>
+    std::vector<T_element> makeVector(std::array<T_element, N_length> aElements)
+    {
+        return std::vector<T_element>{
+            std::make_move_iterator(std::begin(aElements)),
+            std::make_move_iterator(std::end(aElements)),
+        };
+    }
+
+
+    /// @brief Helper to initialize a vector from a "list" of value, accepting move-only types
+    /// @note Because std::vector ctor taking std::initializer_list<> 
+    /// does not allow to initialize a vector of move-only type
+    /// @see https://stackoverflow.com/a/8468817/1027706
+    template <class... VT_element>
+    auto makeVector(VT_element &&... vaElements)
+    {
+        return makeVector(std::to_array({std::forward<VT_element>(vaElements)...}));
     }
 
 } // namespace ad
